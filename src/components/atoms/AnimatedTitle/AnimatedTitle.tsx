076ada2f8a1b437337079420676443fd73e5f800
gpitrella <gabriel98_@hotmail.com>
2022-08-02 16:59:03 -0300

    update

--- src/components/atoms/AnimatedTitle/AnimatedTitle.tsx
+++ src/components/atoms/AnimatedTitle/AnimatedTitle.tsx
@@ -15,9 +15,9 @@ const AnimatedTitle = ({}: Props) => {
 							<div className={classes.stage}>
 								<div className={classes.cubespinner}>
 									<div className={classes.face1}>Gabriel Pitrella</div>
-									<div className={classes.face2}>Full End Developer</div>
+									<div className={classes.face2}>FullStack Developer</div>
+									<div className={classes.face4}>FrontEnd / BackEnd</div>
 									<div className={classes.face3}>Creative</div>
-									{/* <div className={classes.face4}>Exceptional</div> */}
 								</div>
 							</div>
 						</span>
