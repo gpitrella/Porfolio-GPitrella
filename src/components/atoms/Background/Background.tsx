3e6a1bc58fb8407520969306bbade92c3cfd14ed
gpitrella <gabriel98_@hotmail.com>
2022-08-05 10:31:06 -0300

    update

--- src/components/atoms/Background/Background.tsx
+++ src/components/atoms/Background/Background.tsx
@@ -9,16 +9,14 @@ const Background = () => {
             <span id={classes.lionfish}><Lotties type="lionfish" width="180px"/></span>
         <ul className={classes.circles}>
             <span><Lotties type="bubbles2" width="500px"/></span>
+            <span><Lotties type="bubbles" width="300px"/></span>
+            <span><Lotties type="bubbles" width="150px"/></span>
             <li></li>
             <li></li>
             <li></li>
-            <li><Lotties type="bubbles" width="200px"/></li>
             <li></li>
             <li></li>
             <li></li>
-            <li></li>
-            <li></li>
-            <li><Lotties type="bubbles" width="350px"/></li>
             <li id={classes.medusa2}><Lotties type="medusa" width="270px"/></li>
             <li id={classes.medusa1}><Lotties type="medusa" width="250px"/></li>
         </ul>
